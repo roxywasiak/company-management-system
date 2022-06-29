994191eb6e6c5fd12691a82abb658a4f3ae1f9b7
roxywasiak <ruksclone@hotmail.com>
2022-06-29 10:21:38 +0100

    added all the queries

--- src/db/queries.sql
+++ src/db/queries.sql
@@ -17,9 +17,31 @@ SELECT role.title, department.name, role.salary FROM role JOIN department ON rol
 
 
 -- all employees
+
 SELECT CONCAT (emp.first_name, '' , emp.last_name) AS 'USER' , job.title, dept.name, job.salary,
  CONCAT (m.first_name, '' , m.last_name) AS MANAGER 
  FROM employee AS emp JOIN employee AS m ON emp.manager_id = m.id INNER JOIN role job ON role emp.role_id = job.id LEFT JOIN department ON job.department_id;
+
+--  add a new employee
+INSERT INTO employee (firstName, lastName, role_id, manager_id) VALUES("${firstName}", "${lastName}", ${role_id}, ${manager_id})
+
+--  add a new role
+
+INSERT INTO role (title, salary, department_id) VALUES("${title}", ${salary}, ${department_id})
+
+-- add a new department
+
+INSERT INTO department (name) VALUES("${newDepartment}")
+
+-- update the employee role 
+
+UPDATE employee SET role_id = ${role_id} WHERE id = ${id}
+
+
+
+
+
+
 
 
 
---
+++ src/utils/db.js
@@ -0,0 +1,13 @@
+const inquirer = require("inquirer");
+const  = require("")
+// init function
+
+//promise resolve reject
+
+// go through questions
+
+// quit
+// progress false
+
+//call the init()
+init();
