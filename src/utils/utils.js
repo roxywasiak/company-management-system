91ee41bf1110a264a2218b172ec72d74e4450dbf
roxywasiak <ruksclone@hotmail.com>
2022-06-29 10:46:13 +0100

    added basic functions and syntax to show the departments etc

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,7 +1,26 @@
-// generate the user options selected from the tables
-
 // department
+const showDepartmentChoices = (departmentsFromDB) => {
+  return; // get the dept map them //return object key:value key would be the name of dept and value is the id
+};
 
 // role
+const showRoleChoices = (rolesFromDB) => {
+  return;
+};
 
+//manager
+const showManagerChoices = (managersFromDB) => {
+  return;
+};
 // employee
+const showEmployeeChoices = (employeeFromDB) => {
+  return;
+};
+
+// export the functions
+module.exports = {
+  showDepartmentChoices,
+  showRoleChoices,
+  showManagerChoices,
+  showEmployeeChoices,
+};
