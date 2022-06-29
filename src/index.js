89597090232d157ab1953a90baf067960d98ace1
roxywasiak <ruksclone@hotmail.com>
2022-06-29 14:27:01 +0100

    added the update employee functions and questions with queries

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ const { printTable } = require("console-table-printer");
 
 const {
   showDepartmentChoices,
-  showRoleChoices,
   showManagerChoices,
   showEmployeeChoices,
 } = require("./utils/utils");
@@ -35,7 +34,7 @@ const init = async () => {
     //go through your questions js file and follow this route using the if statements
     if (userChoice === "viewEmployee") {
       const employees = await db.query(
-        `
+        `                                   
              SELECT CONCAT (emp.first_name, '' , emp.last_name) AS 'USER' , job.title, dept.name, job.salary,
               CONCAT (m.first_name, '' , m.last_name) AS MANAGER 
               FROM employee AS emp JOIN employee AS m ON emp.manager_id = m.id INNER JOIN role job ON role emp.role_id = job.id LEFT JOIN department ON job.department_id;`
@@ -44,18 +43,74 @@ const init = async () => {
     }
     //add employees
     if (userChoice === "addEmployee") {
+      //queries
       const role = await db.query("SELECT * FROM role");
       const employee = await db.query("SELECT * FROM employee");
+      //questions
+      const employeeQuestions = [
+        {
+          type: "input",
+          message: "Please enter employee's first name:",
+          name: "firstName",
+        },
+        {
+          type: "input",
+          message: "Please enter employee's last name:",
+          name: "lastName",
+        },
+        {
+          type: "list",
+          message: "Please select a role:",
+          name: "role_id",
+          choices: generateRoleChoices(role),
+        },
+        {
+          type: "list",
+          message: "Please select a Manager:",
+          name: "manager_id",
+          choices: generateManagerChoices(employee),
+        },
+      ];
+
+      const { role_id, firstName, lastName, manager_id } =
+        await inquirer.prompt(employeeQuestions);
+
+      await db.query(
+        `INSERT INTO employee (firstName, lastName, role_id, manager_id) VALUES("${firstName}", "${lastName}", ${role_id}, ${manager_id})`
+      );
+      console.log(`You have added ${firstName} ${lastName} to the system`);
+    }
+
+    //update employee
+    if (userChoice === "updateEmployeeRole") {
+      //queries
+      const employee = await db.query("SELECT * FROM employee");
+      const role = await db.query("SELECT * FROM role");
+      //questions
+      const updateEmployeeQuestions = [
+        {
+          type: "list",
+          message: "Which employee would you like to update?",
+          name: "id",
+          choices: generateEmployeeChoices(employee),
+        },
+        {
+          type: "list",
+          message:
+            "What role would you like to assign to the selected employee?",
+          name: "role_id",
+          choices: generateRoleChoices(role),
+        },
+      ];
+      const { id, role_id } = await inquirer.prompt(updateEmployeeQuestions);
+
+      await db.query(
+        `UPDATE employee SET role_id = ${role_id} WHERE id = ${id}`
+      );
+      console.log(`Employee Role has been updated`);
     }
   }
 };
-
-//questions
-//queries
-
-//update employees
-//questions
-//queries
 
 //roles
 //questions
